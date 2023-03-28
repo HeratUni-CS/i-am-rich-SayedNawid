866cd952216644e73f0e00e76dad80389fcabaa2
Sayed Nawid <85921991+SayedNawid@users.noreply.github.com>
2023-03-29 00:40:09 +0430

    Add files via upload
    first commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Center(
+          child: Text("I am rich"),
+        ),
+      ),
+    ),
+  ));
+}
