8ccbc61243dda2439731414fe7f7fda5e7667e15
SayedNawid <sayednawidmohammady@gmail.com>
2023-04-04 09:21:53 +0430

    create appBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Center(
+          child: Text("I am Rich"),
+        ),
+      ),
+    body: Image(image: AssetImage("")),
     ),
   ));
 }
